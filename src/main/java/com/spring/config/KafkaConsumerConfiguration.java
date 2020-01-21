e6580ea72b762cba28f44ff8815531a9c61db236
tariqnawaz <shaikh.tariqnawaz@gmail.com>
2020-01-21 22:05:08 +0400

    kafka configuration

---
+++ src/main/java/com/spring/config/KafkaConsumerConfiguration.java
@@ -0,0 +1,53 @@
+package com.spring.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfiguration {
+    @Autowired
+    Environment env;
+    @Bean
+    KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.setConcurrency(3);
+        factory.getContainerProperties().setPollTimeout(3000);
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        //props.put(ConsumerConfig.CLIENT_ID_CONFIG, env.getProperty("client.id"));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+        return props;
+    }
+}
--- src/main/java/com/spring/rest/HomeResource.java
+++ src/main/java/com/spring/rest/HomeResource.java
@@ -1,17 +1,57 @@
 package com.spring.rest;
 
+import com.spring.SpringbootappApplication;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CountDownLatch;
+
 @RestController
 public class HomeResource {
-	@GetMapping("/")   
+	public static Logger logger = LoggerFactory.getLogger(HomeResource.class);
+	private final CountDownLatch latch = new CountDownLatch(3);
+
+
+	private KafkaTemplate<String, String> template;
+
+	public HomeResource(KafkaTemplate<String, String> template) {
+		this.template = template;
+	}
+
+	@GetMapping("/")
 	public String index() {
+		String topic = "thing1";
+		String msg = "Bismillahi-rehmanir-rahim";
+		template.send(topic,msg);
 		return "Greetings from Spring Boot!";
 	}
+
+
+	@KafkaListener(topics = "myTopic")
 	@GetMapping("/api/home")
     public String homeInit() {
         System.out.println("home controller..home");
         return "Welcome to HOME page of your application.. Alhumdulilah";
     }
+
+	/*@KafkaListener(topics = "thing1")
+	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+		logger.info(cr.toString());
+		latch.countDown();
+	}*/
+
+	@KafkaListener(topics = "thing1")
+	public void listen(@Payload String message) {
+		logger.info("received message='{}'", message);
+	}
+
+
+
 }
